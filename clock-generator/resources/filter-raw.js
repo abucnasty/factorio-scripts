96e3f0c23416aacddd72919c21822629486ed0c7
abucnasty <abucnasty@gmail.com>
2025-12-02 12:39:38 -0500

    ignore raw files

---
+++ clock-generator/resources/filter-raw.js
@@ -0,0 +1,20 @@
+const fs = require("fs/promises");
+
+const category_allow_list = new Set([
+    "item",
+    "recipe",
+])
+
+fs.readFile("./data-raw-dump.json")
+    .then(data => JSON.parse(data.toString("utf-8")))
+    .then(rawData => {
+        const filtered = {}
+
+        for(const category of category_allow_list) {
+            filtered[category] = rawData[category]
+        }
+
+        return fs.writeFile("./data-filtered.json", JSON.stringify(filtered, null, 2))
+    })
+    .then(() => console.log("Done"))
+    .catch(err => console.error(err))
