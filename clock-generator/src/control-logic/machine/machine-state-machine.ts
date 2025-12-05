9f12916ecf2b3fe5c637b1563e3973e06be6cfcb
abucnasty <abucnasty@gmail.com>
2025-12-04 22:21:39 -0500

    set initial machine mode from state

--- clock-generator/src/control-logic/machine/machine-state-machine.ts
+++ clock-generator/src/control-logic/machine/machine-state-machine.ts
@@ -1,5 +1,7 @@
+import assert from "assert"
 import { EntityId } from "../../entities";
-import { EntityState, MachineState, ReadableEntityStateRegistry } from "../../state";
+import { EntityState, MachineState, MachineStatus, ReadableEntityStateRegistry } from "../../state";
+import { TickProvider } from "../current-tick-provider";
 import { ModePlugin, ModeStateMachine, ModeTransitionEvaluator } from "../mode";
 import { MachineIngredientShortageMode, MachineMode, MachineOutputFullMode, MachineWorkingMode } from "./modes";
 import { CraftEventListener, CraftEventListenerPlugin, MachineStatusPlugin } from "./plugins";
@@ -21,19 +23,20 @@ export class MachineStateMachine extends ModeStateMachine<MachineMode> {
         super(initialMode, graph, plugins);
     }
 
-    addCraftEventPlugin(onCraftEvent: CraftEventListener): void {
-        this.addPlugin(new CraftEventListenerPlugin(this.machine_state, onCraftEvent));
+    addCraftEventPlugin(tick_provider: TickProvider, onCraftEvent: CraftEventListener): void {
+        this.addPlugin(new CraftEventListenerPlugin(this.machine_state, tick_provider, onCraftEvent));
     }
 }
 
 
 function create(args: {
     machine_state: MachineState,
+    initial_mode_status?: MachineStatus
     plugins?: ModePlugin<MachineMode>[],
 }): MachineStateMachine {
 
 
-    const { machine_state, plugins = [] } = args;
+    const { machine_state, initial_mode_status = MachineStatus.INGREDIENT_SHORTAGE, plugins = [] } = args;
 
     const working_mode = new MachineWorkingMode(machine_state);
     const ingredient_shortage_mode = new MachineIngredientShortageMode();
@@ -44,6 +47,7 @@ function create(args: {
             ingredient_shortage_mode,
             output_full_mode,
             working_mode,
+            machine_state,
         )],
         [ingredient_shortage_mode, new IngredientShortageModeTransitionEvaluator(
             machine_state,
@@ -55,7 +59,8 @@ function create(args: {
         )],
     ]);
 
-    const initial_mode = ingredient_shortage_mode;
+    const initial_mode = Array.from(graph.keys()).find(mode => mode.status === initial_mode_status)
+    assert(initial_mode != undefined, `Could not find initial mode for machine with id ${machine_state.entity_id} and status ${initial_mode_status}`);
 
     return new MachineStateMachine(
         args.machine_state,
@@ -73,6 +78,7 @@ function createFromId(machine_entity_id: EntityId, entity_state_registry: Readab
     }
     return MachineStateMachine.create({
         machine_state: machine_state,
+        initial_mode_status: machine_state.status,
     });
 }
 
