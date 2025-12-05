cf919f56c6b54e2be887e9f4dad5df0694043224
abucnasty <abucnasty@gmail.com>
2025-12-04 22:20:28 -0500

    fix pickup mode not honoring enable control

--- clock-generator/src/control-logic/inserter/transitions/pickup-mode-transition-evaluator.ts
+++ clock-generator/src/control-logic/inserter/transitions/pickup-mode-transition-evaluator.ts
@@ -1,20 +1,28 @@
 import { InserterState } from "../../../state";
+import { EnableControl } from "../../enable-control";
 import { ModeTransition, ModeTransitionEvaluator } from "../../mode";
 import { InserterMode } from "../modes";
+import { InserterDisabledMode } from "../modes/disabled-mode";
 import { InserterSwingMode } from "../modes/swing-mode";
 
 export class PickupModeTransitionEvaluator implements ModeTransitionEvaluator<InserterMode> {
 
     constructor(
         private readonly inserterState: InserterState,
-        private readonly swing_mode: InserterSwingMode
+        private readonly swing_mode: InserterSwingMode,
+        private readonly disabled_mode: InserterDisabledMode,
+        private readonly enable_control: EnableControl,
     ) {}
 
     public onEnter(fromMode: InserterMode): void {}
 
     public onExit(toMode: InserterMode): void {}
 
-    public evaluateTransition(): ModeTransition<InserterMode>{
+    public evaluateTransition(): ModeTransition<InserterMode> {
+        if(!this.enable_control.isEnabled()) {
+            return ModeTransition.transition(this.disabled_mode, `Inserter disabled while picking up ${this.heldItemName()}`);
+        }
+
         if (this.heldItemQuantity() === this.inserterState.inserter.metadata.stack_size) {
             return ModeTransition.transition(this.swing_mode, `Picked up full stack of ${this.heldItemName()}`);
         }
