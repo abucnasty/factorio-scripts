7f39e323747a6660c733c8e293c970e18374f54c
abucnasty <abucnasty@gmail.com>
2025-12-15 16:43:47 -0500

    fix bug in computing simulation mode

--- clock-generator/src/crafting/sequence/simulation-mode.ts
+++ clock-generator/src/crafting/sequence/simulation-mode.ts
@@ -1,5 +1,5 @@
 import { Duration } from "../../data-types";
-import { Inserter, Machine } from "../../entities";
+import { Inserter, InserterStackSize, Machine } from "../../entities";
 
 export const SimulationMode = {
     LOW_INSERTION_LIMITS: "low_insertion_limits",
@@ -16,20 +16,39 @@ export function computeSimulationMode(
 ): SimulationMode {
     const machine_inputs = Array.from(source_machine.inputs.values());
     const filtered_items = inserter.filtered_items;
-    const any_filtered_item_has_low_insertion_limit = machine_inputs
-        .filter(it => filtered_items.has(it.item_name))
-        .some(it => it.automated_insertion_limit.quantity < inserter.metadata.stack_size)
 
-    if (any_filtered_item_has_low_insertion_limit) {
-        return SimulationMode.LOW_INSERTION_LIMITS;
+    const is_output_inserter = inserter.source.entity_id.id === source_machine.entity_id.id;
+    const is_input_inserter = inserter.sink.entity_id.id === source_machine.entity_id.id;
+
+    if (!is_output_inserter && !is_input_inserter) {
+        throw new Error(`Inserter ${inserter.entity_id} is not connected to machine ${source_machine.entity_id}`);
     }
 
-    const output_block = source_machine.output.outputBlock;
+    if (is_input_inserter) {
+        const any_filtered_item_has_low_insertion_limit = machine_inputs
+            .filter(it => filtered_items.has(it.item_name))
+            .some(it => it.automated_insertion_limit.quantity < InserterStackSize.SIZE_16)
 
-    // if the output block condition is less than the max stack size for the ingredient, need to prevent
-    // desyncs and add a buffer of a pickup to use the inserter as the inventory holder
-    if (output_block.quantity < output_block.max_stack_size) {
-        return SimulationMode.PREVENT_DESYNCS;
+        if (any_filtered_item_has_low_insertion_limit) {
+            return SimulationMode.LOW_INSERTION_LIMITS;
+        }
+    }
+
+    if (is_output_inserter) {
+
+        const any_input_has_low_insertion_limit = machine_inputs.some(it => it.automated_insertion_limit.quantity < InserterStackSize.SIZE_16)
+
+        if(any_input_has_low_insertion_limit) {
+            return SimulationMode.LOW_INSERTION_LIMITS;
+        }
+
+        const output_block = source_machine.output.outputBlock;
+
+        // if the output block condition is less than the max stack size for the ingredient, need to prevent
+        // desyncs and add a buffer of a pickup to use the inserter as the inventory holder
+        if (output_block.quantity < output_block.max_stack_size) {
+            return SimulationMode.PREVENT_DESYNCS;
+        }
     }
 
     return SimulationMode.NORMAL;
