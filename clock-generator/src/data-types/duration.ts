275b1d34561a267b7caafc82b655c6f6c12124ae
abucnasty <abucnasty@gmail.com>
2025-12-20 16:02:28 -0500

    elevate duration to class

--- clock-generator/src/data-types/duration.ts
+++ clock-generator/src/data-types/duration.ts
@@ -3,29 +3,33 @@ import {
     TICKS_PER_SECOND
 } from "./constants"
 
-export interface Duration {
-    ticks: number;
-    seconds: number;
+export class Duration {
+
+    public static ofTicks = ofTicks
+    public static ofSeconds = ofSeconds
+    public static zero = ofTicks(0)
+
+    constructor(
+        public readonly ticks: number,
+        public readonly seconds: number
+    ) {}
+
+    public add(other: Duration): Duration {
+        return Duration.ofTicks(this.ticks + other.ticks);
+    }
 }
 
 
 function ofTicks(ticks: number): Duration {
-    return {
+    return new Duration(
         ticks,
-        seconds: SECONDS_PER_TICK.multiply(ticks).toDecimal()
-    };
+        SECONDS_PER_TICK.multiply(ticks).toDecimal()
+    );
 }
 
 function ofSeconds(seconds: number): Duration {
-    return {
-        ticks: TICKS_PER_SECOND.multiply(seconds).toDecimal(),
-        seconds: seconds
-    };
-}
-
-
-export const Duration = {
-    ofTicks: ofTicks,
-    ofSeconds: ofSeconds,
-    zero: ofTicks(0),
+    return new Duration(
+        TICKS_PER_SECOND.multiply(seconds).toDecimal(),
+        seconds
+    );
 }
